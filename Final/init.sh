e54c1b09987aa86c1bb60831ba95b342a3cfe97a
ashwinkol <ashwinkolkar22@gmail.com>
2022-11-27 18:15:09 +0530

    Finally Done!

--- Final/init.sh
+++ Final/init.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 service mysql start
-mysqld
+mysql
+CREATE USER 'user'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON *.* TO 'user'@'%' WITH GRANT OPTION;
+update mysql.user set host='%' where user='root';
+FLUSH PRIVILEGES;
 
